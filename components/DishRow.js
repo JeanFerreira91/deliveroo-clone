8e29c64e6fbb14cf2ef80602347be8537e4eb980
Jean Ferreira <jeanferreira91@hotmail.com>
2022-08-25 16:19:46 +0100

    RestaurantScreen and DishRow working as expected

---
+++ components/DishRow.js
@@ -0,0 +1,80 @@
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import Currency from 'react-currency-formatter';
+import React, { useState } from 'react';
+import { urlFor } from '../sanity';
+import { MinusCircleIcon, PlusCircleIcon } from 'react-native-heroicons/solid';
+
+const DishRow = ({ id, name, description, price, image }) => {
+    // State to check if a dish has been clicked by the user
+    const [isPressed, setIsPressed] = useState(false);
+
+    return (
+        <>
+        <TouchableOpacity
+            onPress={() => setIsPressed(!isPressed)}
+            className={
+                `
+                    bg-white border p-4 border-gray-200 ${
+                    isPressed && "border-b-0"
+                    }
+                `
+            }
+        >
+            
+            {/* Displaying the Dish information */}
+            <View className="flex-row">
+                <View className="flex-1 pr-2">
+                    <Text className="text-lg mb-1">
+                        {name}
+                    </Text>
+                    <Text className="text-gray-400">
+                        {description}
+                    </Text>
+                    <Text className="text-gray-400 mt-2">
+                        <Currency quantity={price} currency="GBP" />
+                    </Text>
+                </View>
+
+                <View>
+                    <Image
+                        style={{
+                            borderWidth: 1,
+                            borderColor: '#F3F3F4',
+                        }}
+                        source={{ uri: urlFor(image).url() }}
+                        className="h-20 w-20 bg-gray-300 p-4"
+                    />
+                </View>
+            </View>
+
+        </TouchableOpacity>
+        
+        {/* If a dish is clicked by the user, show + and - icons to select dish quantity */}
+        {isPressed && (
+            <View className="bg-white px-4">
+                <View className="flex-row items-center space-x-2 pb-3">
+
+                    <TouchableOpacity>
+                        <MinusCircleIcon
+                            color="#00CCBB"
+                            size={40}
+                        />
+                    </TouchableOpacity>
+                    
+                    <Text>0</Text>
+
+                    <TouchableOpacity>
+                        <PlusCircleIcon
+                            color="#00CCBB"
+                            size={40}
+                        />
+                    </TouchableOpacity>
+
+                </View>
+            </View>
+        )}
+        </>
+    );
+};
+
+export default DishRow
--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -3,6 +3,7 @@ import React from "react";
 import { StarIcon } from "react-native-heroicons/solid";
 import { LocationMarkerIcon } from "react-native-heroicons/outline";
 import { urlFor } from "../sanity";
+import { useNavigation } from "@react-navigation/native";
 
 const RestaurantCard = ({
   id,
@@ -16,8 +17,29 @@ const RestaurantCard = ({
   long,
   lat,
 }) => {
+  // Navigation hook to navigate to the restaurant screen
+  const navigation = useNavigation();
+
   return (
-    <TouchableOpacity className="bg-white mr-3 shadow">
+    <TouchableOpacity
+      // when user taps on the restaurant card, navigate to the restaurant screen
+      // to see the menu of the restaurant. We are passing props to the restaurant screen
+      onPress={() => {
+        navigation.navigate("Restaurant", {
+          id,
+          imgUrl,
+          title,
+          rating,
+          genre,
+          address,
+          short_description,
+          dishes,
+          long,
+          lat,
+        });
+      }}
+      className="bg-white mr-3 shadow"
+    >
       <Image
         source={{
           uri: urlFor(imgUrl).url(),
